0d5250b409d28f5bcfd5cb5f30030305cd7f9a4a
Hirochika Asai <panda@jar.jp>
2020-12-06 19:15:05 +0900

    Add bpsk_tx.py.

---
+++ bpsk_tx.py
@@ -0,0 +1,73 @@
+import SoapySDR
+import sys
+import argparse
+import math
+import numpy as np
+
+# Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--sample-rate', type=int, default=1e6)
+parser.add_argument('--bandwidth', type=int, default=5e6)
+parser.add_argument('--rf', type=int, default=2420e6)
+
+PREAMBLE = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1]
+SYNC = [0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0] # Unique word
+DATA = [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1]
+
+"""
+Main routine
+"""
+def main(args):
+    # Create an SDR device instance
+    try:
+        sdr = SoapySDR.Device(dict(driver="lime"))
+    except:
+        sys.stderr.write("Failed to create an SDR device instance.\n")
+        return False
+
+    if not sdr:
+        sys.stderr.write("Could not find any SDR devices.\n")
+        return False
+
+    # Setup the Tx channel 0
+    sdr.setSampleRate(SoapySDR.SOAPY_SDR_TX, 0, args.sample_rate)
+    sdr.setBandwidth(SoapySDR.SOAPY_SDR_TX, 0, args.bandwidth)
+    sdr.setAntenna(SoapySDR.SOAPY_SDR_TX, 0, "BAND1")
+    sdr.setGain(SoapySDR.SOAPY_SDR_TX, 0, 50.0)
+    sdr.setFrequency(SoapySDR.SOAPY_SDR_TX, 0, args.rf)
+
+    # Setup a Tx stream
+    txStream = sdr.setupStream(SoapySDR.SOAPY_SDR_TX, SoapySDR.SOAPY_SDR_CF32, [0])
+    # Activate the stream
+    sdr.activateStream(txStream)
+
+    # Data to send
+    data = PREAMBLE + SYNC + DATA
+
+    # Check the maximum transmit unit
+    mtu = sdr.getStreamMTU(txStream)
+    if mtu < len(data):
+        sys.stderr.write("MTU is too small {} < {}\n".format(mtu, len(data)))
+        return False
+
+    # Build preamble, sync code (unique word) and data
+    samples = np.exp( 1j * math.pi * np.array(data, np.complex64) ).astype(np.complex64)
+    # Transmit the samples
+    status = sdr.writeStream(txStream, [samples], samples.size, timeoutUs=1000000)
+    if status.ret != samples.size:
+        sys.stderr.write("Failed to transmit all samples in writeStream(): {}\n".format(status.ret))
+        return False
+
+    # Deactivate and close the stream
+    sdr.deactivateStream(txStream)
+    sdr.closeStream(txStream)
+
+
+"""
+Call the main routine
+"""
+if __name__ == "__main__":
+    # Parse the arguments
+    args = parser.parse_args()
+    main(args)
+
